d0ad4ced87ee1259a9b9e9853458303d1eb12baf
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-04-04 15:05:16 -0300

    Update Restaurante.cs

--- codigo/RestaurantePOG/Restaurante.cs
+++ codigo/RestaurantePOG/Restaurante.cs
@@ -8,35 +8,53 @@ namespace RestaurantePOG
     /// Classe representando o restaurante
     /// </summary>
     public class Restaurante
+{
+	#region atributos
+	private List<Requisicao> queue;
+	private List<Mesa> mesas;
+	private List<Cliente> clientes;
+	private int maxMesas = 10;
+	#endregion
+
+	#region Construtores
+	public Restaurante()
 	{
-		Requisicao [] requisicao;
-		Queue <FilaEspera> FilaEspera;
-
-        /// <summary>
-        /// Método para atender um cliente no restaurante
-        /// </summary>
-        /// <param name="cliente">Cliente a ser atendido</param>
-        public void atenderCliente(Cliente cliente)
-		{
-			throw new NotImplementedException();
-		}
-
-        /// <summary>
-        /// Método privado para adicionar um cliente à fila de espera
-        /// </summary>
-        /// <param name="cliente">Cliente a ser adicionado à fila de espera</param>
-        private void addFilaEspera(Cliente cliente)
-		{
-			throw new NotImplementedException();
-		}
-
-        /// <summary>
-        /// Método para atender o próximo cliente da fila de espera
-        /// </summary>
-        /// <returns>O cliente atendido</returns>
-		public Cliente atenderFilaEspera()
-		{
-			throw new NotImplementedException();
-		}
+	
 	}
+    #endregion
+
+    #region mÃ©todos
+    public void atenderCliente(Cliente cliente, int qntPessoas)
+	{
+
+	}
+
+   
+    public void addFilaEspera(Cliente cliente)
+	{
+
+	}
+
+    
+	public void atenderFilaEspera(Cliente cliente)
+	{
+
+	}
+
+	public void procurarNaFila()
+	{
+
+	}
+
+	public void encerrarAtendimento(int idRequisicao)
+	{
+
+	}
+
+
+	public void reservarMesa(Mesa mesa)
+	{
+
+	}
+Â Â Â Â #endregion
 }
