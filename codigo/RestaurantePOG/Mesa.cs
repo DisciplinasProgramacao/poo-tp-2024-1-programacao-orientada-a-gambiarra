3c1294655c7c84706967a56ce301c73c422a02ae
Matheus Costa <64905221+MatheusCS1901@users.noreply.github.com>
2024-04-03 23:08:51 -0300

    Add files via upload
    Fiz a classe mesa, porem ela precisa de algumas coisas da classe cliente

--- codigo/RestaurantePOG/Mesa.cs
+++ codigo/RestaurantePOG/Mesa.cs
@@ -9,17 +9,49 @@ namespace RestaurantePOG
     /// </summary>
     public class Mesa
 	{
-		int id;
-		int capacidade;
-		bool status;
+    private int id;
+    private int capacidade;
+    private bool status;
+    private Queue<FilaEspera> filaEspera; /// fila de espera de clientes que desejam ocupar a mesa
+        public Mesa(int id, int capacidade)
+    {
+        this.id = id;
+        this.capacidade = capacidade;
+        this.status = false;
+        this.filaEspera = new Queue<FilaEspera>();
+    }
+        public void AtenderCliente(Cliente cliente)/// Atribui um cliente a mesa e marca a mesa como ocupada
+    {
+        this.cliente = cliente;
+        this.status = true;
+    }
 
-        /// <summary>
-        /// Método para verificar o status de ocupação da mesa
-        /// </summary>
-        /// <returns>True se a mesa estiver ocupada, False caso contrário</returns>
-        public bool statusOcupacao()
+        public bool statusOcupacao()///Retorna true se a mesa estiver ocupada e false se estiver disponivel
 		{
-			throw new NotImplementedException();
+			 return status;
 		}
-	}
+        public void AtendimentoEncerrado()///Marca a mesa com disponivel e libera o cliente que estava nela 
+    {
+        this.status = false;
+        this.cliente = null;
+    }
+ public int Id
+    {
+        get { return id; }
+    }
+
+    public int Capacidade
+    {
+        get { return capacidade; }
+    }
+
+    public Cliente Cliente
+    {
+        get { return cliente; }
+    }
+    public Queue<FilaEspera> FilaEspera
+    {
+        get { return filaEspera; }
+    }
+}
 }
