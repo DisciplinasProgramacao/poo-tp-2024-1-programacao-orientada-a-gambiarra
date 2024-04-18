29131eea9cdef9213320d1a8f403b2770d311fea
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-04-18 04:30:23 -0300

    Update Restaurante.cs

--- codigo/RestaurantePOG/Restaurante.cs
+++ codigo/RestaurantePOG/Restaurante.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace RestaurantePOG
@@ -10,51 +12,80 @@ namespace RestaurantePOG
     public class Restaurante
 {
 	#region atributos
-	private List<Requisicao> queue;
-	private List<Mesa> mesas;
-	private List<Cliente> clientes;
+	private List<Requisicao> requisicao = new List<Requisicao>();
+	private List<Mesa> mesas = new List<Mesa>();
+	private Queue<Cliente> filaClientes = new Queue<Cliente>();
+	private Queue<Cliente> filaEspera = new Queue<Cliente>();
 	private int maxMesas = 10;
 	#endregion
 
 	#region Construtores
-	public Restaurante()
+	public Restaurante(List<Mesa> mesas)
 	{
-	
+		this.mesas = mesas;
+		this.filaClientes = new Queue<Cliente>();
+		this.filaEspera = new Queue<Cliente>();
+		this.maxMesas = 10;
+		this.requisicao = new List<Requisicao>();
 	}
     #endregion
 
     #region métodos
     public void atenderCliente(Cliente cliente, int qntPessoas)
 	{
-
+		Console.WriteLine("Atendido: " + filaClientes.Peek());
+		filaClientes.Dequeue();
+		cliente.solicitarReserva(qntPessoas);
 	}
 
    
-    public void addFilaEspera(Cliente cliente)
+    private void addFilaEspera(Cliente cliente)
 	{
-
+		 filaEspera.Enqueue(cliente);
+         Console.WriteLine("Cliente adicionado à fila de espera.");
 	}
 
     
-	public void atenderFilaEspera(Cliente cliente)
+	private void atenderFilaEspera(Cliente cliente, int qntPessoas)
 	{
-
-	}
-
-	public void procurarNaFila()
-	{
-
+		Console.WriteLine("Atendido: " + filaEspera.Peek());
+		filaEspera.Dequeue();
+		cliente.solicitarReserva(qntPessoas);
 	}
 
 	public void encerrarAtendimento(int idRequisicao)
 	{
-
+		
 	}
 
 
 	public void reservarMesa(Mesa mesa)
 	{
+		mesa.isOcupada();
+	}
 
+	private String verificarMesaDisponivel()
+	{
+		 StringBuilder listaMesas = new StringBuilder();
+            foreach(Mesa mesa in mesas)
+            {
+                listaMesas.AppendLine(String.Format("Capacidade Mesa: {0} Disponibilidade: {1}"+ mesa.getCapacidade(), mesa.statusOcupacao()));
+            }
+            return listaMesas.ToString();
+	}
+	public String relatorioFila(Cliente cliente)
+	{
+		StringBuilder relatorioFila = new StringBuilder();
+		foreach	(Cliente obj in filaClientes)
+		{
+			relatorioFila.Append(obj.ToString() + "(Fila de atendimento)");
+		}
+		foreach	(Cliente obj in filaEspera)
+		{
+			relatorioFila.Append(obj.ToString() + "(Fila de espera)");
+		}
+		return relatorioFila.ToString();
 	}
     #endregion
+}
 }
