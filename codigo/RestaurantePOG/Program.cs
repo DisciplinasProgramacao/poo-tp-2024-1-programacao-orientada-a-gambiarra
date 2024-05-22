ff80e672a79fe86238dea752de845f6c880d8678
Arthur Grossi <arthursouzagrossi1@gmail.com>
2024-05-21 22:24:07 -0300

    Alteração Program.cs e Inclusao UML v3.1

--- codigo/RestaurantePOG/Program.cs
+++ codigo/RestaurantePOG/Program.cs
@@ -1,90 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace RestaurantePOG
-{
-    public class Program
-    {
-        #region Região de Variáveis Globais
-        static List<Cliente> clientes = new List<Cliente>();
-        #endregion
 
-        #region Região dos Métodos do Menu
-        /// <summary>
-        /// Cadastra cliente em uma lista.
-        /// </summary>
-        public static void cadastrarCliente()
-        {
-         
-            Console.Write("Favor inserir o nome do cliente: ");
-            string nome = Console.ReadLine();
+namespace RestaurantePOG {
+    public class Program{
+        static void Main(string[] args) {
 
+            #region Variáveis
+            Restaurante restaurante = new Restaurante("POG - Comidinhas Veganas");
+            bool continuar = true;
+            int opcao;
+            #endregion
+
+            while(continuar) {
+                ExibeMenuPrincipal();
+                opcao = EscolheOpcao(1, 5);
+                Console.Clear();
             
-            Console.Write("A reserva é para quantas pessoas? ");
-            int quantPessoa = int.Parse(Console.ReadLine());
-
-            // Se o usuário inserir um número válido, cria um novo cliente.
-            Cliente cliente = new Cliente(nome, quantPessoa);
-            clientes.Add(cliente);
-
-            // Exibe as informações do cliente cadastrado na lista criada.
-            Console.WriteLine("\n!!!Cliente Cadastrado!!!\n");
-            Console.WriteLine(cliente.ToString() + "\n");
-           
-        }
-        /// <summary>
-        /// Imprimi a lista de clientes
-        /// </summary>
-        public static void listarClientes()
-        {
-            // Usa Stream para Ordena a lista de clientes pelo ID em ordem crescente através do Sort
-            clientes.Sort((c1, c2) => c1.Id.CompareTo(c2.Id));
-
-            // Exibe os clientes ordenados
-            foreach (Cliente cliente in clientes)
-            {
-                Console.WriteLine(cliente.ToString());
+                switch(opcao) {
+                    case 1:
+                        CadastrarCliente();
+                        break;
+                    case 2:
+                        AtenderCliente();
+                        break;
+                    case 3:
+                        MostrarCardapio();
+                        break;
+                    case 4:
+                        IncluirItemCardapio();
+                        break;
+                    case 5:
+                        continuar = false;
+                        Console.WriteLine("Encerrando Programa...");
+                        System.Threading.Thread.Sleep(500);
+                        break;
+                }
             }
         }
 
-        /// <summary>
-        /// Imprimi o menu do restaurante.
-        /// </summary>
-        /// <returns></returns>
-        public static int opMenu()
-        {
-
-            Console.WriteLine("======= MENU =======");
+        /// <summary> Exibe o menu principal do restaurante </summary>
+        public static void ExibeMenuPrincipal() {
+            Console.WriteLine("==============================");
+            Console.WriteLine("======= MENU PRINCIPAL ======="); 
+            Console.WriteLine("==============================\n");
             Console.WriteLine("1 - Cadastrar Cliente.");
-            Console.WriteLine("2 - Exibir lista de clientes.");
-            Console.WriteLine("3 - Sair do programa.");
+            Console.WriteLine("2 - Atender Cliente.");
+            Console.WriteLine("3 - Mostrar Cardápio");
+            Console.WriteLine("4 - Incluir Item no Cardápio");
+            Console.WriteLine("5 - Encerrar Programa.\n");
             
-            return int.Parse(Console.ReadLine()); ;
+        }
+
+        /// <summary>Método que solicita que o usuário digite um valor numérico e verifica se o mesmo é um valor que está dentro de um intervalo especificado</summary>
+        /// <param name="min">Valor mínimo dentro do intervalo</param>
+        /// <param name="max">Valor maximo dentro do intervalo</param>
+        /// <returns>retorna a opçãpo selecionada em um numero inteiro</returns>
+        private static int EscolheOpcao(int min, int max) {
+            bool valido = false;
+            int numero;
+
+            while(!valido){
+                numero = DigitaInteiro();
+                if (numero >= min && numero <= max){ valido = true; }
+            }
+            return numero;    
+        }
+
+        /// <summary>Verifica se o um numero a ser digitado é inteiro. Repete o processo até conseguir um numero válido</summary>
+        /// <returns>Retorna um numero inteiro válido</returns>
+        private static int DigitaInteiro(){
+            bool numerico, valido = false;
+            string input;
+            int inteiro;
+
+            while(!valido){ 
+                input = Console.ReadLine();
+                numerico = int.TryParse(Console.ReadLine());
+                if (numerico) { inteiro = int.Parse(input); valido = true; }
+                else { Console.Write("Digite um valor valor válido: "); }
+            }
+            return inteiro;
+        }
+
+        /// <summary>Realiza o Cadastro do cliente no restaurante</summary>
+        public static void CadastrarCliente() {
+            Console.Write("Informe o nome do cliente: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("Insira a quantidade de pessoas: ");
+            int qtdPessoas = DigitaInteiro();
+
+            //Gera uma nova requisição de um novo cliente.
+            Requisicao novaRequisicao = new Requisicao(nome, qtdPessoas);
+            restaurante.AdicionarRequisicao(novaRequisicao);
+            Console.WriteLine("\nCliente Cadastrado!");     
+        }
+
+        public static void AtenderCliente() { 
+            List<Requisicao> emAtendimento = restaurante.ConsultarEmAtendimento();
+            int contador = 1;
+
+            Console.WriteLine("Selecione o cliente que deseja atender: ");
+            foreach(Requisicao requisicao in emAtendimento) {
+                Console.WriteLine(contador + " - " + requisicao.Cliente.nome);
+                contador++;
+            }
+
+            opcao = EscolheOpcao(1, emAtendimento.Count());
+            Requisicao requisicao = emAtendimento[--opcao];
+        }
+
+        public static void MostrarCardapio() { 
 
         }
 
-        #endregion
-
-        static void Main(string[] args)
-        {
-            int op =0;
-
-            do { 
-                op = opMenu();
-                switch (op) {
-                    case 1:
-                        cadastrarCliente();
-                        break;
-                    case 2:
-                        Console.WriteLine("==== Lista de Clientes ====");
-                        listarClientes();
-                        Console.WriteLine();
-                        break;
-                    case 3:
-                        Console.WriteLine("Programa Encerrado...");
-                        break;
-                }
-            } while (op != 3);
+        public static void IncluirItemCardapio() {
 
         }
     }
