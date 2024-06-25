97c10916be2362601f9fb6ba78bdedd488f4f4d5
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-24 23:22:23 -0300

    Update Pedido.cs

--- codigo/RestaurantePOG/Pedido.cs
+++ codigo/RestaurantePOG/Pedido.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace RestaurantePOG {
     public class Pedido {
-
+        #region Atributos
         private Item item;
         private int quantidade;
         private double valorUnitario;
+        #endregion
 
+        #region Construtor
         public Pedido(int quantidade, Item item)  {
             
             this.item = item;
             this.quantidade = quantidade;
             valorUnitario = item.getPreco();
         }
+        #endregion
         
+        #region Métodos
         /// <summary>Retorna o valor total do pedido.</summary>
         /// <returns>Total dos pedidos</returns>
         public double valorTotal() { return valorUnitario * quantidade; }
+        #endregion
         
     }
 }
