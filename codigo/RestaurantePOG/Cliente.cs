3bedad9b7e684f994aeb7966cd5e80e51728b8a0
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-24 23:17:41 -0300

    region

--- codigo/RestaurantePOG/Cliente.cs
+++ codigo/RestaurantePOG/Cliente.cs
@@ -5,16 +5,20 @@ namespace RestaurantePOG
 {
     /// <summary>Classe representando o cliente do restaurante.</summary>
     public class Cliente {
+        #region Atributos
         private string nome;
-
+        #endregion
+        #region Construtor
         public Cliente(string nome) { this.nome = nome; }    
-
+        #endregion
+        #region Métodos
        /// <summary>Retorna o nome do Cliente</summary>
        /// <returns>String contendo o nome do cliente</returns>
         public string getNome(){ return nome; }
 
         public override string ToString() { return $"-> {nome}"; }
 
+        #endregion
     }
 }
 
