3a20e5983bfc60f552b832b296ff80d1f76891cd
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-26 21:27:49 -0300

    Comentando métodos

--- codigo/RestaurantePOG/Pedido.cs
+++ codigo/RestaurantePOG/Pedido.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace RestaurantePOG {
     public class Pedido {
         #region Atributos
@@ -13,6 +7,9 @@ namespace RestaurantePOG {
         #endregion
 
         #region Construtor
+        /// <summary>Construtor de Pedido</summary>
+        /// <param name="quantidade">Quantidade de itens solicitados</param>
+        /// <param name="item">O item solicitado</param>
         public Pedido(int quantidade, Item item)  {
             this.item = item;
             this.quantidade = quantidade;
@@ -24,7 +21,9 @@ namespace RestaurantePOG {
         /// <summary>Retorna o valor total do pedido.</summary>
         /// <returns>Total dos pedidos</returns>
         public double valorTotal() { return valorUnitario * quantidade; }
-
+        
+        /// <summary>Transforma Pedido em uma string</summary>
+        /// <returns>Uma string com as informações do Pedido de forma formatada</returns>
         public override string ToString() {
             return $"{item.getNome()} ***** x{quantidade}: R$ {valorTotal()}";
         }
