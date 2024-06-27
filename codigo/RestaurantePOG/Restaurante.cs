7979b1874bda46d5380882f628eae19605dc5630
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-26 21:12:01 -0300

    Comentando métodos

--- codigo/RestaurantePOG/Restaurante.cs
+++ codigo/RestaurantePOG/Restaurante.cs
@@ -7,15 +7,15 @@ using System.Linq;
 using System.Reflection;
 
 namespace RestaurantePOG {
-    ///<summary>Classe representando o restaurante </summary>
+    ///<summary>Classe representando o restaurante</summary>
     public class Restaurante : Estabelecimento{
         #region Atributos
         private FilaEspera fila_espera;
         #endregion
 
         #region Construtor
-        ///<summary>Método responsável por instanciar um novo objeto da classe Restaurante</summary>
-        ///<param name="nome"></param>
+        ///<summary>Construtor do restaurante</summary>
+        ///<param name="nome">Nome do restaurante</param>
         ///<returns>Objeto do tipo Restaurante</returns>
         public Restaurante(string nome) : base(nome) {
             fila_espera = new FilaEspera();
@@ -23,7 +23,8 @@ namespace RestaurantePOG {
         #endregion
 
         #region Métodos
-
+        /// <summary>Atende o cliente especificado. Inicia sua requisição e o acomoda em uma mesa.</summary>
+        /// <param name="nomeCliente"></param>
         public override void atenderCliente(Requisicao requisicao) {
             // Procurar mesas livres
             List<Mesa> mesas_disponiveis = mesas.Where(m => !m.estahOcupada()).ToList();
@@ -37,7 +38,7 @@ namespace RestaurantePOG {
                 }
             }    
         }
-
+        /// <summary>Atende o próximo cliente da fila de espera, por ordem de chegada, porém é atendido apenas se tiver mesa disponível para a quantidade de pessoas</summary>
         public void atenderProximo() {
             bool alocado = false;
             Requisicao? requisicao;
@@ -62,7 +63,8 @@ namespace RestaurantePOG {
                 if (alocado) { break; } // Interrompe o loop de clientes assim que uma requisição é alocada
             }
         }
-
+        /// <summary>Exibe uma lista dos clientes que estão alocados em uma mesa (prontos para serem atendidos)</summary>
+        /// <returns>lista de clientes em atendimento .ToString()</returns>
         public override string exibeListaAtendimento()  {
             StringBuilder listaAtendimento = new StringBuilder();
 
@@ -74,7 +76,9 @@ namespace RestaurantePOG {
 
             return listaAtendimento.ToString();            
         }
-
+        /// <summary>Cadastra um cliente, porém, se a quantidade de pessoas exceder a quantidade de pessoas que cabe nas mesas do restaurante, o cliente não é cadastrado, pois nunca poderá ser atendido.<summary>
+        /// <param name="nome">Nome do cliente</param>
+        /// <param name="qtdPessoas">Quantidade de pessoas</param>
         public override void cadastrarCliente(string nome, int qtdPessoas){
             // Gerar uma requisicao com as informações especificadas
             Cliente cliente = new (nome);
