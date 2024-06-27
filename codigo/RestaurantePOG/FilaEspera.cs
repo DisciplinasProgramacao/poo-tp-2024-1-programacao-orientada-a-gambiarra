e9e229910ec0ea6a79cd808873145e7e1816d01b
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-26 21:44:35 -0300

    Comentando métodos

--- codigo/RestaurantePOG/FilaEspera.cs
+++ codigo/RestaurantePOG/FilaEspera.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 
 namespace RestaurantePOG { 
-    /// <summary>
-    /// Classe representando a fila de espera do restaurante
-    /// </summary>
+    /// <summary>Classe representando a fila de espera do restaurante</summary>
     public class FilaEspera : IEnumerable<Cliente>
     {
         #region Atributos
@@ -13,9 +9,7 @@ namespace RestaurantePOG {
         #endregion
 
         #region Construtor
-        /// <summary>
-        /// Construtor padrão da classe FilaEspera
-        /// </summary>
+        /// <summary>Construtor da classe FilaEspera</summary>
         public FilaEspera()
         {
             clientes = new List<Cliente>();
@@ -23,14 +17,24 @@ namespace RestaurantePOG {
         #endregion
 
         #region Métodos
+        /// <summary>Adiciona um cliente à lista de espera</summary>
+        /// <param name="cliente">O objeto cliente do tipo Cliente</param>
         public void adicionarCliente(Cliente cliente) {  clientes.Add(cliente); }
-
+        
+        /// <summary>Remove um cliente específica da lista de espera</summary>
+        /// <param name="cliente">Cliente a ser removido</param>
         public void removerFila(Cliente cliente) { clientes.Remove(cliente); }
-
+        
+        /// <summary>Informa se a fila de espera está vazia</summary>
+        /// <returns>True se sim, false se não</returns>
         public bool estaVazia() { return clientes.Count == 0; }
-
+        
+        /// <summary>Método responsável por retornar o enumerator de clientes.</summary>
+        /// <returns>Enum de clientes</returns>
         public IEnumerator<Cliente> GetEnumerator() { return clientes.GetEnumerator(); }
-
+        
+        /// <summary>Método responsável por retornar uma Enum.</summary>
+        /// <returns>Enum</returns>
         IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
         #endregion
     }
