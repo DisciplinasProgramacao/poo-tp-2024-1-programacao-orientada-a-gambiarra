3d8a661bc7a5ecede0f9b158ce47a0fa231b7e64
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-26 22:16:29 -0300

    Comentando métodos

--- codigo/RestaurantePOG/Comanda.cs
+++ codigo/RestaurantePOG/Comanda.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
 
 namespace RestaurantePOG
 {
@@ -26,6 +20,9 @@ namespace RestaurantePOG
         }
         #endregion
 
+        /// <summary>Método responsável por realizar o pedido da comanda</summary>
+        /// <param name="pedido">O pedido específico</param>
+        /// <returns>Se o pedido foi realizado ou não</returns>
         public bool realizarPedido(Pedido pedido) {
             bool pedidoRealizado = false;
 
@@ -37,16 +34,27 @@ namespace RestaurantePOG
             return pedidoRealizado;
         }
 
+        /// <summary>Método responsável por calcular o valor da conta, considerando a taxa de serviço</summary>
+        /// <returns>Valor da conta + a taxa de serviço</returns>
         public double calcularValorConta() { return calcularTotalPedidos() + calcularTaxaServico(); }
 
+        /// <summary>Método responsável por calcular a taxa de serviço</summary>
+        /// <returns>o valor da taxa de serviço</returns>
         public double calcularTaxaServico() { return valorTaxaServico = calcularTotalPedidos() * TAXA_SERVICO; }
 
+        /// <summary>Método responsável por calcular o valor total dos pedidos</summary>
+        /// <returns>Valor total dos pedidos</returns>
         public double calcularTotalPedidos() { return valorTotalItens = listaPedidos.Select(l => l.valorTotal()).Sum(); }
 
+        /// <summary>Método responsável por fechar a comanda</summary>
         public void fecharComanda() { comandaFechada = true; }
 
+        /// <summary>Método responsável por retornar o valor total da comanda</summary>
+        /// <returns>Valor total dos itens da comanda</returns>
         public double getValorTotal(){ return valorTotalItens; }
-
+        
+        /// <summary>Mostra os pedidos em lista</summary>
+        /// <returns>Retorna uma string contendo todos os pedidos</returns>
         public string listarPedidos() {   
             StringBuilder sb = new StringBuilder();
             foreach (Pedido pedido in listaPedidos){ sb.AppendLine(pedido.ToString()); }
