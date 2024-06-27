bc3ef114b8824b2c6a7bfa6890caa277f89296a0
pedroluckeroth <51711866+pedroluckeroth@users.noreply.github.com>
2024-06-26 22:18:03 -0300

    Comentando métodos

--- codigo/RestaurantePOG/Cliente.cs
+++ codigo/RestaurantePOG/Cliente.cs
@@ -1,21 +1,23 @@
-using System;
-using System.Text;
-
 namespace RestaurantePOG
 {
-    /// <summary>Classe representando o cliente do restaurante.</summary>
+    /// <summary>Classe representando o cliente do restaurante</summary>
     public class Cliente {
         #region Atributos
         private string nome;
         #endregion
+            
         #region Construtor
         public Cliente(string nome) { this.nome = nome; }    
         #endregion
+            
         #region Métodos
        /// <summary>Retorna o nome do Cliente</summary>
        /// <returns>String contendo o nome do cliente</returns>
         public string getNome(){ return nome; }
 
+        
+        /// <summary>Nome formatado em string</summary>
+        /// <returns>O Nome formatado em string</returns>
         public override string ToString() { return $"-> {nome}"; }
 
         #endregion
